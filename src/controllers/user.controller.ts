62cd007b2927927c88486e4906edd2369b07c15c
Glauco Villas Boas <glappsmobile@gmail.com>
2021-12-13 04:08:41 -0300

    created custom class errors for user route

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,13 +1,26 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import * as userService from '../services/user.service';
+import { statusCode } from '../enums/httpStatus';
 
-const createUser = async (req: Request, res: Response) => {
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name } = req.body;
   const userClass = req.body.class;
 
-  const createdUser = await userService.createUser(name, userClass);
+  try {
+    const createdUser = await userService.createUser(name, userClass);
 
-  res.send(createdUser);
+    res.send(createdUser);
+  } catch (error) {
+    if (error.name === 'UserError') {
+      return res.status(statusCode.INTERNAL_SERVER_ERROR).send(error.message);
+    }
+
+    if (error.name === 'SessionError') {
+      return res.status(statusCode.INTERNAL_SERVER_ERROR).send(error.message);
+    }
+
+    next(error);
+  }
 };
 
 export {
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,12 +2,14 @@ import jwt from 'jsonwebtoken';
 import * as userRepository from '../repositories/users.repository';
 import * as sessionRepository from '../repositories/session.repository';
 import { Session } from '../interfaces/session.interface';
+import UserError from '../errors/UserError';
+import SessionError from '../errors/SessionError';
 
 const createUser = async (name: string, userClass: string): Promise<Session> => {
   const user = await userRepository.create(name, userClass);
 
-  if (user === null) {
-    // erro
+  if (user) {
+    throw new UserError('An unexpected error occurred.');
   }
 
   const token = jwt.sign(
@@ -16,6 +18,10 @@ const createUser = async (name: string, userClass: string): Promise<Session> => 
   );
 
   const session = await sessionRepository.create(user.id, token);
+
+  if (session === null) {
+    throw new SessionError('An unexpected error occurred.');
+  }
 
   return session;
 };
